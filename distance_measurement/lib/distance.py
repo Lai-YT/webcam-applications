42550a2e4422259c9085ea1267573f8e0dff2457
Lai-YT <381xvmvbib@gmail.com>
2021-07-08 17:28:44 +0800

    dis. measure.: extract distance bar showing method

--- distance_measurement/lib/distance.py
+++ distance_measurement/lib/distance.py
@@ -3,6 +3,7 @@ from numpy import ndarray
 from typing import Tuple, Union
 
 from .color import *
+from .cv_font import *
 
 '''
 Need some more adjustment on the distance_level of "energy bar".
@@ -64,7 +65,7 @@ def face_data(image: ndarray) -> Tuple[int, Union[ndarray, Tuple], Tuple[int, in
     gray_image: ndarray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     faces: Union[ndarray, Tuple] = (
-        face_detector.detectMultiScale(gray_image, 1.3, 5))  # ndarray[int, int, int, int]
+        face_detector.detectMultiScale(gray_image, 1.3, 5))  # type: ndarray[int, int, int, int]
     face_width: int = 0
     face_center: Tuple[int, int] = None
 
@@ -75,18 +76,11 @@ def face_data(image: ndarray) -> Tuple[int, Union[ndarray, Tuple], Tuple[int, in
     return face_width, faces, face_center
 
 
-def draw_face_area(image: ndarray, faces: ndarray, distance_level: int,
-                   *, call_out: bool = False) -> None:
-    '''Draw Rectangle and display the distance over Screen.
+def draw_face_area(image: ndarray, faces: ndarray) -> None:
+    '''Draw a Rectangle to indicate the face.
     @param
       image: simply the frame
-      face:
-        upper-left x, upper-left y, height of face, width of face
-      distance_level:
-        which change the line according the distance changes (Interactivate)
-    @keyword
-      call_out = False:
-        whether to show distance and Rectangle on the Screen or not
+      face: upper-left x, upper-left y, height of face, width of face
     '''
 
     for (x, y, h, w) in faces:
@@ -102,14 +96,27 @@ def draw_face_area(image: ndarray, faces: ndarray, distance_level: int,
         cv2.line(image, (x, y+h), (x, y+h-LLV), (GREEN), line_thickness)
         cv2.line(image, (x+w, y+h), (x+w, y+h-LLV), (GREEN), line_thickness)
 
-        if distance_level < 10:
-            distance_level = 10
+
+def show_distance_bar(image: ndarray, faces: ndarray, distance: float) -> None:
+    '''Show a bar above the face.
+    The closer the face is, the longer the inner bar is.
+    @param
+      image: simply the frame
+      face: upper-left x, upper-left y, height of face, width of face
+      distance_level:
+        which change the line according the distance changes (Interactivate)
+    '''
+    distance = round(distance, 2)
+    distance_level = int(distance)
+    if distance_level < 10:
+        distance_level = 10
+
+    for (x, y, h, w) in faces:
+        cv2.line(image, (x, y-11), (x+180, y-11), (ORANGE), 28)
+        cv2.line(image, (x, y-11), (x+180, y-11), (YELLOW), 20)
+        cv2.line(image, (x, y-11), (x+180-distance_level, y-11), (GREEN), 18)
 
         face_center: Tuple[int, int] = (int(w/2)+x, int(h/2)+y)
-        if call_out:
-            # cv2.line(image, (x, y), face_center, (GRAY), 1)
-            cv2.line(image, (x, y-11), (x+180, y-11), (ORANGE), 28)
-            cv2.line(image, (x, y-11), (x+180, y-11), (YELLOW), 20)
-            cv2.line(image, (x, y-11), (x+180-distance_level, y-11), (GREEN), 18)
-
-            cv2.circle(image, face_center, 5, (MAGENTA), 1)
+        cv2.circle(image, face_center, 5, (MAGENTA), 1)
+        cv2.putText(image, f'distance {distance} cm',
+                    (x-6, y-6), font_3, 0.5, (BLACK), 2)
