fa1578a8539ab45ba6c6aaf6dddfe46efedef232
Lai-YT <381xvmvbib@gmail.com>
2021-07-08 23:30:24 +0800

    dis. measure.: funtion rename and add comments

--- distance_measurement/face_distance_track.py
+++ distance_measurement/face_distance_track.py
@@ -17,25 +17,13 @@ call_out: bool = True
 # warns when the distance is shorter than threshold
 warning: int = 45  # centi-meter
 
-
-'''
-getting informations from reference image
-'''
-ref_image: ndarray = cv2.imread(ref_image_path)
-ref_image_face_width: int = 0
-for (_, _, w, _) in face_data(ref_image):
-    ref_image_face_width = w
-focal_length_found = focal_length(
-    known_distance, ref_image_face_width, known_width)
+focal_length_found: float = (
+    get_img_focal_length(ref_image_path, known_distance, known_width))
 
 
 if __name__ == '__main__':
     # Camera Object
     cap = cv2.VideoCapture(0)  # Number According to your Camera
-
-    # Define the codec and create VideoWriter object
-    # fourcc: int = cv2.VideoWriter_fourcc(*'XVID')
-    # out = cv2.VideoWriter('face_distance.avi', fourcc, 30.0, (640, 480))
     while True:
         _, frame = cap.read()
 
@@ -43,20 +31,19 @@ if __name__ == '__main__':
 
         # finding the distance by calling function distance finder
         for (face_x, face_y, face_w, face_h) in faces:
-            if face_w != 0:
-                draw_face_area(frame, faces)
-                if call_out:
-                    distance: float = distance_finder(
-                        focal_length_found, known_width, face_w)
-                    show_distance_bar(
-                        frame, faces, distance, warning_threshold=warning)
-
+            if face_w == 0:
+                continue
+            draw_face_area(frame, faces)
+            if call_out:
+                distance: float = estimate_distance(
+                    focal_length_found, known_width, face_w)
+                show_distance_bar(
+                    frame, faces, distance, warning_threshold=warning)
         cv2.imshow('frame', frame)
-        # out.write(frame)
-
-        if cv2.waitKey(1) == ord('q'):
+        key: int = cv2.waitKey(5)
+        # q: end detection
+        if key == ord('q'):
             break
 
     cap.release()
-    # out.release()
     cv2.destroyAllWindows()
--- distance_measurement/lib/distance.py
+++ distance_measurement/lib/distance.py
@@ -5,34 +5,38 @@ from typing import Tuple, Union
 from lib.color import *
 from lib.cv_font import *
 
-'''
-Need some more adjustment on the distance_level of "distance bar".
-'''
+
+def get_img_focal_length(image_path: str, distance: float, width: float) -> float:
+    '''Get the focal length(distance between lens to CMOS sensor) in the img.
+    @param
+      image_path: absolue path of the image
+      distance: real distance between object and camera
+      width: real width of the object
+    @return: the focal length
+    '''
+    image: ndarray = cv2.imread(image_path)
+    object_width: int = 0
+    for (_, _, w, _) in face_data(image):
+        object_width = w
+    return focal_length(distance, width, object_width)
 
 
 # focal length finder function
-def focal_length(distance_in_rf_img: float,
-                 width_in_rf_img: float,
-                 real_width: float) -> float:
-    '''This function Calculate the focal length(distance between lens to CMOS sensor).
+def focal_length(distance: float, width: float, width_in_rf_img: float) -> float:
+    '''Calculate the focal length(distance between lens to CMOS sensor).
     @param
-      distance_in_rf_img:
-        distance measured from object to the Camera while Capturing Reference image
-      width_in_rf_img:
-        object width in the frame /image in our case in the reference image(found by Face detector)
-      real_width:
-        Actual width (cm) of object, in real world
-    @retrun
-        focal_length
+      distance: real distance between object and camera
+      width_in_rf_img: object width in the frame /image in our case in the
+                       reference image(found by Face detector)
+      width: The actual width (cm) of object in real world
+    @retrun: the focal length
     '''
-    return (width_in_rf_img * distance_in_rf_img) / real_width
+    return (width_in_rf_img * distance) / width
 
 
 # distance estimation function
-def distance_finder(focal_length: float,
-                    real_width: float,
-                    width_in_frame: float) -> float:
-    '''This Function simply Estimates the distance between object and camera.
+def estimate_distance(focal_length: float, real_width: float, width_in_frame: float) -> float:
+    '''Estimates the distance between object and camera.
     @param
       focal_length: may be the result of the focal_length function
       real_width: Actual width (cm) of object, in real world
@@ -49,12 +53,10 @@ def face_data(image: ndarray) -> Tuple[int, Union[ndarray, Tuple]]:
     @param
       image: simply the frame
     @return
-      face_width:
-        width of face in the frame, which allow us to calculate the distance and
-        find focal length
-      faces:
-        upper-left x, upper-left y, width of face, height of face,
-        empty tuple if no face detected
+      face_width: width of face in the frame, which allow us to calculate the
+                  distance and find focal length
+      faces: upper-left x, upper-left y, width of face, height of face,
+             empty tuple if no face detected
     '''
     # face detector object
     face_detector = cv2.CascadeClassifier(
@@ -69,7 +71,7 @@ def face_data(image: ndarray) -> Tuple[int, Union[ndarray, Tuple]]:
 
 
 def draw_face_area(image: ndarray, faces: ndarray) -> None:
-    '''Draw a Rectangle to indicate the face.
+    '''Draw angles to indicate the face.
     @param
       image: simply the frame
       face: upper-left x, upper-left y, width of face, height of face
@@ -77,20 +79,21 @@ def draw_face_area(image: ndarray, faces: ndarray) -> None:
 
     for (x, y, w, h) in faces:
         line_thickness: int = 2
+        # affects the length of corner line
         LLV = int(h*0.12)
 
-        # cv2.rectangle(image, (x, y), (x+w, y+h), BLACK, 1)
+        # vertical corner lines
         cv2.line(image, (x, y+LLV), (x+LLV, y+LLV), (GREEN), line_thickness)
         cv2.line(image, (x+w-LLV, y+LLV), (x+w, y+LLV), (GREEN), line_thickness)
         cv2.line(image, (x, y+h), (x+LLV, y+h), (GREEN), line_thickness)
         cv2.line(image, (x+w-LLV, y+h), (x+w, y+h), (GREEN), line_thickness)
 
+        # horizontal corner lines
         cv2.line(image, (x, y+LLV), (x, y+LLV+LLV), (GREEN), line_thickness)
         cv2.line(image, (x+w, y+LLV), (x+w, y+LLV+LLV),
                  (GREEN), line_thickness)
         cv2.line(image, (x, y+h), (x, y+h-LLV), (GREEN), line_thickness)
         cv2.line(image, (x+w, y+h), (x+w, y+h-LLV), (GREEN), line_thickness)
-
 
 
 def show_distance_bar(image: ndarray, faces: ndarray, distance: float,
@@ -100,9 +103,13 @@ def show_distance_bar(image: ndarray, faces: ndarray, distance: float,
     @param
       image: simply the frame
       face: upper-left x, upper-left y, width of face, height of face
-      distance_level:
-        which change the line according the distance changes (Interactivate)
+      distance: real distance between the user and the screen
+    @keyword
+      warning_threshold: When the face is closer than the threshold,
+                         bar color turns into red and warning massage shows.
+                         0 implicitly means no warning.
     '''
+
     warning_threshold = round(warning_threshold, 2)
     distance = round(distance, 2)
     distance_level = int(distance)
@@ -119,13 +126,15 @@ def show_distance_bar(image: ndarray, faces: ndarray, distance: float,
             cv2.line(image, (x, y-45), (x+100, y-45), (RED), 22)
             cv2.line(image, (x, y-11), (x+180, y-11), (RED), 18)
         else:
+            # 120 with empty inner bar, full if shorter than the warning_threshold
             cv2.line(image, (x, y-11),
                      (max(x, int(x+(120-distance_level)*180/(120-warning_threshold))), y-11),
                      (GREEN), 18)
-            # cv2.line(image, (x, y-11), (x+180-distance_level, y-11), (GREEN), 18)
-
+        # small circle at the center of face
         face_center: Tuple[int, int] = (int(w/2)+x, int(h/2)+y)
         cv2.circle(image, face_center, 5, (MAGENTA), 1)
+
+        # distance bar normal message
         cv2.putText(image, f'distance {distance} cm',
                     (x-3, y-6), font_2, 0.6, (BLACK), 1)
 
