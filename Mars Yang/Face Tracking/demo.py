0fccfbe04e164b685dce98714aafd31628e33cd7
marsyang <ianfirst123@gmail.com>
2021-07-12 20:58:31 +0800

    Gaze Tracking

---
+++ Mars Yang/Face Tracking/demo.py
@@ -0,0 +1,106 @@
+import cv2
+import numpy
+import os
+import time
+
+from lib.color import *
+from lib.cv_font import *
+from lib.distance import *
+from lib.gaze_tracking import GazeTracking
+from lib.timer import *
+
+
+def draw_gazing_direction(image: numpy.ndarray, gaze: GazeTracking) -> None:
+    """Draw where the user is gazing at: right, left, center or blinking.
+
+    Arguments:
+        image (numpy.ndarray): The image to draw on
+        gaze (GazeTracking): The gaze dealing with
+    """
+
+    text: str = ""
+    if gaze.is_blinking():
+        text = "Blinking"
+    elif gaze.is_right():
+        text = "Looking right"
+    elif gaze.is_left():
+        text = "Looking left"
+    elif gaze.is_center():
+        text = "Looking center"
+
+    cv2.putText(image, text, (0, 60), FONT_3, 1, BLUE, 2)
+
+
+def draw_time_stamp(image: numpy.ndarray, timer: Timer) -> None:
+    """Draw the time of the timer on the upper right of the image.
+
+    Arguments:
+        image (numpy.ndarray): The image to draw on
+        timer (Timer): The timer which records the time
+    """
+
+    time_duration: str = f"duration {(timer.time() // 60):02d}:{(timer.time() % 60):02d}"
+    cv2.putText(image, time_duration, (432, 20), FONT_3, 0.8, BLUE, 1)
+
+
+"""variables (should be set by the user)"""
+face_to_cam_dist_in_ref: float = 45  # centi-meter
+personal_face_width: float = 12.5  # centi-meter
+ref_image_path: str = "img/ref_img.jpg"
+to_draw_dist_bar: bool = True
+warn_dist: int = 30  # centi-meter
+
+cwd: str = os.path.abspath(os.path.dirname(__file__))
+ref_image_path = os.path.abspath(os.path.join(cwd, ref_image_path))
+
+focal_length_in_ref: float = (
+    get_img_focal_length(ref_image_path, face_to_cam_dist_in_ref, personal_face_width))
+
+"""initialize the detection objects"""
+gaze = GazeTracking()
+webcam = cv2.VideoCapture(0)
+timer = Timer()
+
+timer.start()
+
+while True:
+    _, original_frame = webcam.read()
+
+    """do gaze tracking"""
+    # We send this frame to GazeTracking to analyze it
+    gaze.refresh(original_frame)
+
+    # This method returns a frame copy to draw on,
+    # remember to make sure all following methods draw on the same frame.
+    output_frame: numpy.ndarray = gaze.annotated_frame()
+
+    draw_gazing_direction(output_frame, gaze)
+
+    """do distance measurement"""
+    faces: numpy.ndarray = face_data(original_frame)
+
+    # If gaze is not detected, pause the timer
+    if not gaze.pupils_located:
+        timer.pause()
+        cv2.putText(output_frame, "timer paused", (432, 40), FONT_3, 0.6, RED, 1)
+    else:
+        timer.start()
+
+    for (x, y, w, h) in faces:
+        draw_face_area(output_frame, faces)
+        if to_draw_dist_bar:
+            distance: float = estimate_distance(focal_length_in_ref, personal_face_width, w)
+            draw_distance_bar(output_frame, faces, distance, threshold=warn_dist)
+
+    """record screen focus time"""
+    draw_time_stamp(output_frame, timer)
+    
+    """show result"""
+    cv2.imshow("demo", output_frame)
+    # ESC
+    if cv2.waitKey(1) == 27:
+        break
+
+webcam.release()
+timer.reset()
+cv2.destroyAllWindows()
---
+++ Mars Yang/Face Tracking/face_distance_track.py
@@ -0,0 +1,46 @@
+import cv2
+from numpy import ndarray
+
+from lib.distance import *
+
+
+"""variables (should be set by the user)"""
+# distance from camera to object(face) measured
+known_distance: float = 45  # centi-meter
+# mine is 14.3 something, measure your face width, or google it
+known_width: float = 12.5  # centi-meter
+# a reference image of your face for calculating the distance
+ref_image_path: str = "img/ref_img.jpg"
+# To show the distance bar or not.
+call_out: bool = True
+# warns when the distance is shorter than threshold
+warning: int = 30  # centi-meter
+
+focal_length_found: float = (
+    get_img_focal_length(ref_image_path, known_distance, known_width))
+
+# Camera Object
+webcam = cv2.VideoCapture(0)  # Number According to your Camera
+
+while True:
+    _, frame = webcam.read()
+
+    faces: ndarray = face_data(frame)  # type: ndarray[int, int, int, int]
+
+    # finding the distance by calling function distance finder
+    for (face_x, face_y, face_w, face_h) in faces:
+        if face_w == 0:
+            continue
+        draw_face_area(frame, faces)
+        if call_out:
+            distance: float = estimate_distance(
+                focal_length_found, known_width, face_w)
+            draw_distance_bar(
+                frame, faces, distance, threshold=warning)
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
