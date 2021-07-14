010f2cf2f7866622e5df956d618abb307dbb3080
Lai-YT <381xvmvbib@gmail.com>
2021-07-14 13:46:01 +0800

    FaceDistanceDetector can now draw face angles

--- Lai-YT/demo.py
+++ Lai-YT/demo.py
@@ -4,41 +4,11 @@ import os
 
 from lib.color import *
 from lib.cv_font import *
-from lib.distance import *
+from lib.draw import *
+from lib.face_distance_detector import FaceDistanceDetector
 from lib.gaze_tracking import GazeTracking
 from lib.timer import Timer
 from lib.video_writer import VideoWriter
-
-
-def draw_gazing_direction(image: numpy.ndarray, gaze: GazeTracking) -> None:
-    """Draw where the user is gazing at: right, left, center or blinking.
-
-    Arguments:
-        image (numpy.ndarray): The image to draw on
-        gaze (GazeTracking): The gaze dealing with
-    """
-    text: str = ""
-    if gaze.is_blinking():
-        text = "Blinking"
-    elif gaze.is_right():
-        text = "Looking right"
-    elif gaze.is_left():
-        text = "Looking left"
-    elif gaze.is_center():
-        text = "Looking center"
-
-    cv2.putText(image, text, (0, 60), FONT_3, 1, BLUE, 2)
-
-
-def draw_time_stamp(image: numpy.ndarray, timer: Timer) -> None:
-    """Draw the time of the timer on the upper right of the image.
-
-    Arguments:
-        image (numpy.ndarray): The image to draw on
-        timer (Timer): The timer which records the time
-    """
-    time_duration: str = f"duration {(timer.time() // 60):02d}:{(timer.time() % 60):02d}"
-    cv2.putText(image, time_duration, (432, 20), FONT_3, 0.8, BLUE, 1)
 
 
 """variables (should be set by the user)"""
@@ -51,57 +21,57 @@ warn_dist: int = 45  # centi-meter
 cwd: str = os.path.abspath(os.path.dirname(__file__))
 ref_image_path = os.path.abspath(os.path.join(cwd, ref_image_path))
 
-focal_length_in_ref: float = (
-    get_img_focal_length(ref_image_path, face_to_cam_dist_in_ref, personal_face_width))
-
 """initialize the detection objects"""
-gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
+distance_detector = FaceDistanceDetector(cv2.imread(ref_image_path),
+                                         face_to_cam_dist_in_ref,
+                                         personal_face_width)
+gaze = GazeTracking()
 timer = Timer()
 
-output_video_path: str = os.path.abspath(os.path.join(cwd, "output_video.avi"))
-# Due to the slow writing rate, the fps can't be too high and might be machine depending.
-video_writer = VideoWriter(output_video_path, fps=7)
+# output_video_path: str = os.path.abspath(os.path.join(cwd, "output_video.avi"))
+# # Due to the slow writing rate, the fps can't be too high and might be machine depending.
+# video_writer = VideoWriter(output_video_path, fps=7)
 
 timer.start()
 
-while True:
-    _, original_frame = webcam.read()
+while webcam.isOpened():
+    _, frame = webcam.read()
+
+    """do distance measurement"""
+    distance_detector.estimate(frame)
+    frame = distance_detector.annotated_frame()
+
+    text: str = ""
+    color: BGR = None
+    if distance_detector.has_face:
+        distance = distance_detector.distance()
+        text = str(round(distance, 2))
+    else:
+        text = "No face detected."
+    cv2.putText(frame, text, (60, 60), FONT_3, 0.9, MAGENTA, 1)
 
     """do gaze tracking"""
     # We send this frame to GazeTracking to analyze it
-    gaze.refresh(original_frame)
-
-    # This method returns a frame copy to draw on,
-    # remember to make sure all following methods draw on the same frame.
-    output_frame: numpy.ndarray = gaze.annotated_frame()
-    draw_gazing_direction(output_frame, gaze)
-
-    """do distance measurement"""
-    faces: numpy.ndarray = face_data(original_frame)
-
-    if len(faces) == 0 and not gaze.pupils_located:
-        timer.pause()
-        cv2.putText(output_frame, "timer paused", (432, 40), FONT_3, 0.6, RED, 1)
-    else:
-        timer.start()
-    for (x, y, w, h) in faces:
-        draw_face_area(output_frame, faces)
-        if to_draw_dist_bar:
-            distance: float = estimate_distance(focal_length_in_ref, personal_face_width, w)
-            draw_distance_bar(output_frame, faces, distance, threshold=warn_dist)
+    gaze.refresh(frame)
+    frame = gaze.annotated_frame()
 
     """record screen focus time"""
-    draw_time_stamp(output_frame, timer)
+    if not distance_detector.has_face and not gaze.pupils_located:
+        timer.pause()
+        cv2.putText(frame, "timer paused", (432, 40), FONT_3, 0.6, RED, 1)
+    else:
+        timer.start()
+    draw_time_stamp(frame, timer)
 
     """show result"""
-    cv2.imshow("demo", output_frame)
-    video_writer.write(output_frame)
+    cv2.imshow("demo", frame)
+    # video_writer.write(frame)
     # ESC
     if cv2.waitKey(1) == 27:
         break
 
 webcam.release()
-video_writer.release()
+# video_writer.release()
 timer.reset()
 cv2.destroyAllWindows()
