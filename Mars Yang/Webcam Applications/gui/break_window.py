38e9a8959ae460ab250d607f6fcc4a5a2e4c59b3
marsyang <ianfirst123@gmail.com>
2021-09-05 22:15:32 +0800

    something wrong

--- Mars Yang/Webcam Applications/gui/break_window.py
+++ Mars Yang/Webcam Applications/gui/break_window.py
@@ -14,21 +14,30 @@ class BreakGui(QDialog):
         # Set some main window's properties.
         self.setWindowTitle("Break Time!!")
         self.setWindowIcon(QIcon(":webcam.ico"))
-        self.setFixedSize(700, 300)
+        self.setFixedSize(500, 200)
         # Set the central widget and the general layout.
         self._general_layout = QVBoxLayout()
         # self._central_widget = QWidget(parent=self)
         # self._central_widget.setLayout(self._general_layout)
         # self.setCentralWidget(self._central_widget)
-        
-        self._break()
+        break_layout = QFormLayout()
+        break_text = "It's time to take a break!"
+        self.break_message = Label(text=break_text, font_size=20)
+        # self.break_message.setAlignment(Qt.AlignCenter)
+        self.countdown_message = Label(font_size=20)
+        # self.countdown_message.setAlignment(Qt.AlignCenter)
+        break_layout.addRow(self.break_message)
+        break_layout.addRow(self.countdown_message)
+
+        self._general_layout.addLayout(break_layout)
+
 
     def _break(self):
         break_layout = QFormLayout()
         break_text = "It's time to take a break!"
         self.break_message = Label(text=break_text, font_size=20)
         self.break_message.setAlignment(Qt.AlignCenter)
-        self.countdown_message = MessageLabel(font_size=20)
+        self.countdown_message = Label(font_size=20)
         self.countdown_message.setAlignment(Qt.AlignCenter)
         break_layout.addRow(self.break_message)
         break_layout.addRow(self.countdown_message)
