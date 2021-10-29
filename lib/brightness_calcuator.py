fab9fd733eebadb8e12e8fc5be247fa6a54c7c4d
Lai-YT <381xvmvbib@gmail.com>
2021-10-29 15:53:22 +0800

    Merge brightness_calculator.py

--- lib/brightness_calcuator.py
+++ lib/brightness_calcuator.py
@@ -18,7 +18,6 @@ class BrightnessCalculator:
     """Handle processes which require value modulation."""
 
     def __init__(self) -> None:
-        # Init previous weighted value.
         self._pre_weighted_value: Optional[float] = None
         self._base_value: Optional[int] = None
         self._brightness_value: Optional[float] = None
@@ -32,16 +31,19 @@ class BrightnessCalculator:
 
         Arguments:
             mode: Mode affects algorithm used in calculation.
-            base_value: The base value of brightness (The value before checking the checkbox).
+            current_base_value:
+                The base value of brightness (The value before checking the
+                checkbox).
             frames:
-                Image of the corresponding brightness mode.
+                Frame of the corresponding brightness mode.
                 If mode is BOTH, there should have two frames.
         """
-        # Init variables of calculator
-        if self._base_value == None:
+        # Take the values of the first time as the initial values.
+        if self._base_value is None:
             self._base_value = current_base_value
-        if self._brightness_value == None:
+        if self._brightness_value is None:
             self._brightness_value = current_base_value
+
         # Prepare the brightness precentage of the frame(s) that will be used
         # under the corresponding mode.
         if mode in (BrightnessMode.WEBCAM, BrightnessMode.BOTH):
@@ -50,9 +52,8 @@ class BrightnessCalculator:
             screenshot_brightness: int = BrightnessCalculator.get_brightness_percentage(frames[BrightnessMode.COLOR_SYSTEM])
         if mode is BrightnessMode.BOTH:
             # webcam frame takes the lead
-            weighted_frame_brightness: float = _weighted_sum(
-                (webcam_frame_brightness, 0.6),
-                (100-screenshot_brightness, 0.4)
+            weighted_frame_brightness: float = (
+                webcam_frame_brightness * 0.6 + (100-screenshot_brightness) * 0.4
             )
 
         # All remain weights are on self._pre_weighted_value.
@@ -62,7 +63,6 @@ class BrightnessCalculator:
             BrightnessMode.BOTH: ("weighted_frame_brightness", 0.4),
         }
 
-        # locals() returns the local_vars with type Dict[str, Any]
         frame_brightness: float = locals()[weight_of_modes[mode][0]]
         brightness_weight: float = weight_of_modes[mode][1]
         if self._pre_weighted_value is None:
@@ -71,24 +71,23 @@ class BrightnessCalculator:
             # Set _pre_weighted_value as new_weighted_value to avoid NoneType Error.
             self._pre_weighted_value = new_weighted_value
         else:
-            new_weighted_value = _weighted_sum(
-                (frame_brightness, brightness_weight),
-                (self._pre_weighted_value, 1-brightness_weight)
+            new_weighted_value = (
+                frame_brightness * brightness_weight
+                + self._pre_weighted_value * (1-brightness_weight)
             )
         # Both value differences affect the brightness value.
-        base_value_difference = current_base_value - self._base_value
-        weighted_value_difference = int(new_weighted_value - self._pre_weighted_value)
+        base_value_diff: float = current_base_value - self._base_value
+        weighted_value_diff: float = new_weighted_value - self._pre_weighted_value
 
         # Add value difference effect on current brightness value and store the value.
         # Higher the brightness if the environment is bright to keep the screen clear and
         # lower the brightness if the background of screen is light colored.
         if mode in (BrightnessMode.WEBCAM, BrightnessMode.BOTH):
-            self._brightness_value += base_value_difference + weighted_value_difference * 0.25
+            self._brightness_value += base_value_diff + weighted_value_diff * 0.25
         elif mode is BrightnessMode.COLOR_SYSTEM:
-            self._brightness_value += base_value_difference - weighted_value_difference * 0.25
+            self._brightness_value += base_value_diff - weighted_value_diff * 0.25
+        self._brightness_value = _clamp(self._brightness_value, 0, 100)
 
-        # Clamp brightness value
-        self._brightness_value = clamp(self._brightness_value, 0, 100)
         # Update previous value.
         self._base_value = current_base_value
         self._pre_weighted_value = new_weighted_value
@@ -107,26 +106,26 @@ class BrightnessCalculator:
         hue, saturation, value = cv2.split(hsv)  # can be gotten with hsv[:, :, 2] - the 3rd channel
         return int(100 * value.mean() / 255)
 
-    def clear(self):
+    def clear(self) -> None:
         """Clear the variables to make every time auto optimization triggered independent."""
         self._pre_weighted_value = None
         self._base_value = None
         self._brightness_value = None
 
+
 # outer utilities
 
-def _weighted_sum(*number_and_weights: Tuple[float, float]) -> float:
-    """Returns the weighted sum of each (number, weight) pair."""
-    weighted_sum: float = 0
-    for num, weight in number_and_weights:
-        weighted_sum += num * weight
-    return weighted_sum
+def _clamp(value: float, v_min: float, v_max: float) -> float:
+    """Clamps the value into the range [v_min, v_max].
 
-def clamp(num: float, min: float, max: float) -> float:
-    """Returns the clamped value within given boundary."""
-    if num < min:
-        return min
-    elif num > max:
-        return max
-    else:
-        return num
+    e.g., _clamp(50, 20, 40) returns 40.
+    v_min should be less or equal to v_max. (v_min <= v_max)
+    """
+    if not v_min < v_max:
+        raise ValueError("v_min is the lower bound, which should be smaller than v_max")
+
+    if value > v_max:
+        value = v_max
+    elif value < v_min:
+        value = v_min
+    return value
