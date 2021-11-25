1382d1dbdacda56c2acbac5d160bb2979089da7a
MarsYang <ianfirst123@gmail.com>
2021-11-25 23:53:10 +0800

    Add continue command in *fuzzy.py*

--- fuzzy.py
+++ fuzzy.py
@@ -6,16 +6,16 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
 
-blink = ctrl.Antecedent(np.arange(30), "blink")
-body = ctrl.Antecedent(np.arange(10), "body")
-grade = ctrl.Consequent(np.arange(10), "grade")
+blink = ctrl.Antecedent(np.arange(31), "blink")
+body = ctrl.Antecedent(np.arange(11), "body")
+grade = ctrl.Consequent(np.arange(11), "grade")
 
 blink.automf(3)
 body.automf(3)
 
 grade["low"] = fuzz.trimf(grade.universe, [0, 0, 5])
-grade["medium"] = fuzz.trimf(grade.universe, [0, 5, 9])
-grade["high"] = fuzz.trimf(grade.universe, [5, 9, 9])
+grade["medium"] = fuzz.trimf(grade.universe, [0, 5, 10])
+grade["high"] = fuzz.trimf(grade.universe, [5, 10, 10])
 
 rule1 = ctrl.Rule(body["poor"] | blink["poor"], grade["low"])
 rule2 = ctrl.Rule(blink["average"], grade["medium"])
@@ -26,9 +26,14 @@ grading_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
 grading = ctrl.ControlSystemSimulation(grading_ctrl)
 
 while True:
-    grading.input["blink"] = float(input("blink (0 ~ 29) = "))
-    grading.input["body"] = float(input("body (0 ~ 9) = "))
+    grading.input["blink"] = float(input("blink (0 ~ 30) = "))
+    grading.input["body"] = float(input("body (0 ~ 10) = "))
 
     grading.compute()
 
-    print(f"grade (0 ~ 9) = {grading.output['grade']}")
+    print(f"Your concentration grade is {round(grading.output['grade'], 2)}. (0 ~ 10)\n")
+    grade.view(sim=grading)
+
+    command = input("Continue? (Y/N): ")
+    if command == "N":
+        break
