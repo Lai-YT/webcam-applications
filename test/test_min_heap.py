d179bf721dd07c16e3f6af962cfbe18cecdf2d3b
Lai-YT <381xvmvbib@gmail.com>
2022-01-09 09:26:26 +0800

    Introduce MinHeap
    In a single thread application, implement MinHeap with heapq module
    would have better efficiency in comparison with the PriorityQueue in
    queue module.

---
+++ test/test_min_heap.py
@@ -0,0 +1,20 @@
+import unittest
+
+from util.heap import MinHeap
+
+
+class MinHeapTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.min_heap = MinHeap()
+
+    def test_min_always_top(self) -> None:
+        for i in range(100_000, -1, -1):
+            self.min_heap.push(i)
+            self.assertEqual(self.min_heap.min, i)
+
+        for i in range(100_000):
+            self.assertEqual(self.min_heap.pop(), i)
+
+
+if __name__ == "__main__":
+    unittest.main()
