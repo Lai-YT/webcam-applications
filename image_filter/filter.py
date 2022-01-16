ce699a6f071728e285abcd95a8d86dc9132fc96d
MarsYang <ianfirst123@gmail.com>
2022-01-16 16:45:16 +0800

    Enable to get brightness with face area masked

--- image_filter/filter.py
+++ image_filter/filter.py
@@ -3,12 +3,14 @@ import dlib
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.ma as ma
+
 from imutils import face_utils
 from nptyping import NDArray
+from numpy.ma.core import MaskedConstant
 
 from util.image_type import ColorImage
 
-
+np.set_printoptions(threshold=np.inf)
 class ImageFilter:
     """Handle processes which filter the brightness of the image."""
 
@@ -24,7 +26,6 @@ class ImageFilter:
         self._image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # start process
         self._detect_face()
-        self._mask_face_area()
         self._get_brightness()
 
     def plot_image(self) -> None:
@@ -39,25 +40,50 @@ class ImageFilter:
         """Detects face in a image and stores data of the face."""
         self._faces: dlib.rectangles = self._face_detector(self._image)
 
-    def _mask_face_area(self) -> None:
-        # doesn't handle multiple faces
-        if len(self._faces) == 1:
-            fx, fy, fw, fh = face_utils.rect_to_bb(self._faces[0])
-
     def _get_brightness(self) -> None:
-        """Returns the mean of brightness of an image.
-
-        Arguments:
-            image: The image to perform brightness calculation on.
-            mask:  If mask is True, the brightest 10% area of the image
-                   will be filtered.
-        """
+        """Returns the filtered mean of brightness of an image."""
         hsv = cv2.cvtColor(self._image, cv2.COLOR_BGR2HSV)
         # Value is as known as brightness.
         hue, saturation, value = cv2.split(hsv)  # can be gotten with hsv[:, :, 2] - the 3rd channel
-        self._brightness = round(100 * self._filtered_mean(value) / 255, 2)
+
+        mask = self._generate_mask(value)
+        # array of "value" channel with face area masked
+        masked_value = self._mask_face_area(value, mask)
+        self._brightness = round(100 * self._filtered_mean(masked_value) / 255, 2)
+
+    def _generate_mask(self, ndarray: NDArray) -> NDArray:
+        """Get the boundaries of face area and generate the array with
+        corresponding elements masked.
+        
+        Note: 
+            The size of the mask should be the same as "one" channel of an image,
+        passing the size of self._image leads to size error because its size is three
+        times larger than the "value" channel of hsv. 
+        """
+        # doesn't handle multiple faces
+        if len(self._faces) == 1:
+            fx, fy, fw, fh = face_utils.rect_to_bb(self._faces[0])
+            cv2.rectangle(self._image, (fx, fy), (fx+fw, fy+fh), (255, 0, 255), 1)
+        # generate mask with face area masked
+        mask = np.zeros(ndarray.shape)
+        for i in range(fy, fy+fh+1):
+            for j in range(fx, fx+fx+1):
+                mask[i][j] = 1
+        return mask
+
+    def _mask_face_area(self, ndarray: NDArray, mask: NDArray) -> NDArray:
+        return ma.masked_array(ndarray, mask)
 
     @staticmethod
     def _filtered_mean(ndarray: NDArray) -> float:
+        """Filter out the brightest and darkest 5% area of a masked image."""
+        # Flatten the array to 1D and sort it in increasing order.
         sorted_arr = np.sort(ndarray, axis=None)
-        return sorted_arr[:int(len(sorted_arr) * 0.9)].mean()
+        # Cut the masked value to fix the size of the masked array.
+        arr = []
+        for element in sorted_arr:
+            if type(element) != MaskedConstant:
+                arr.append(element)
+        # Convert the array to ndarray to use the mean method.
+        arr = np.array(arr)
+        return arr[int(arr.size * 0.05):int(arr.size * 0.95)].mean()
