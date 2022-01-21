5abfebf85c194bed10d6e3fd98d0df5f61a27423
Lai-YT <381xvmvbib@gmail.com>
2022-01-21 21:04:15 +0800

    Remove plot from `ImageFilter` & demo real time
    Plotting are handled by helper and main functions.

--- image_filter/filter.py
+++ image_filter/filter.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Optional
+from typing import Any, Optional
 
 import cv2
 import dlib
@@ -7,91 +7,93 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.ma as ma
 from imutils import face_utils
-from nptyping import NDArray
+from nptyping import NDArray, UInt8
 
 from util.color import MAGENTA
 from util.image_type import ColorImage
 
 
-# See https://numpy.org/doc/stable/reference/generated/numpy.set_printoptions.html#numpy-set-printoptions
-np.set_printoptions(threshold=sys.maxsize)
-
-
 class ImageFilter:
     """Handles processes which filters the brightness of the image."""
-    # init face detector
-    _face_detector: dlib.fhog_object_detector = dlib.get_frontal_face_detector()
 
     def __init__(self) -> None:
         self._image: Optional[ColorImage] = None
-        self._faces: Optional[dlib.rectangles] = None
-        self._brightness_without_mask: float = 0
-        self._brightness: float = 0
+        self._face: Optional[dlib.rectangle] = None
+        self._value: Optional[NDArray[(Any, Any), UInt8]] = None
 
-    def refresh_image(self, image: ColorImage) -> None:
+    @property
+    def image(self) -> ColorImage:
+        return self._image
+
+    def refresh_image(self, image: ColorImage, face: dlib.rectangle) -> None:
         """Refreshes the image in the filter and starts the process of filtering."""
         self._image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # start process
-        self._detect_face()
-        self._get_brightness()
-
-    def plot_image(self) -> None:
-        """Plots the image and shows filtered brightness on the window."""
-
-        diff = self._brightness - self._brightness_without_mask
-        title = (f"Brightness without mask: {self._brightness_without_mask}\n"
-                 f"Result brightness: {self._brightness} (diff = {diff:.2f})")
-
-        plt.imshow(self._image)
-        plt.title(title)
-        plt.show()
-
-    def _detect_face(self) -> None:
-        """Detects face in a image and stores data of the face."""
-        self._faces = self._face_detector(self._image)
-
-    def _get_brightness(self) -> None:
-        """Returns the filtered mean of brightness of an image."""
-        hsv = cv2.cvtColor(self._image, cv2.COLOR_BGR2HSV)
+        self._face = face
         # Value is as known as brightness.
-        hue, saturation, value = cv2.split(hsv)  # can be gotten with hsv[:, :, 2] - the 3rd channel
+        _, _, self._value = cv2.split(cv2.cvtColor(self._image, cv2.COLOR_BGR2HSV))
 
-        self._brightness_without_mask = round(100 * self._filtered_mean(value) / 255, 2)
-        # array of "value" channel with face area masked
-        masked_array = self._get_masked_array(value)
-        self._brightness = round(100 * self._filtered_mean(masked_array) / 255, 2)
+    def get_brightness(self, mask: bool = True) -> None:
+        """Returns the filtered mean of brightness of the image.
 
-    def _generate_mask(self, array: NDArray) -> NDArray:
-        """Gets the boundaries of face area and generates the array with
+        The brightness is rounded to two decimal places.
+
+        Arguments:
+            mask: Whether the brightest and darkest 5% area of the image is
+                  excluded or not. True in default.
+        """
+        if mask:
+            # array of "value" channel with face area masked
+            masked_arr = self._get_value_with_face_masked()
+            # compress the masked array to truncate masked constants
+            data_arr = masked_arr.compressed()
+        else:
+            data_arr = self._value.flatten()
+        return round(100 * self._filtered_mean(data_arr) / 255, 2)
+
+    def _generate_face_mask(self) -> NDArray:
+        """Gets the boundaries of face area and generates the value with
         corresponding elements masked.
         """
         # Note: The size of the mask should be the same as a single channel of
         # an image, passing the size of self._image leads to size error because
         # its size is three times larger than the "value" channel of hsv.
-        if self._faces is None:
+        if self._face is None or self._value is None:
             raise ValueError("please refresh the image first")
-        if len(self._faces) > 1:
-            raise ValueError("multiple faces aren't allowed")
-        if not self._faces:
+        if self._face.is_empty():
             # all-pass for a no face image
-            return np.zeros(array.shape, dtype=np.bool8)
+            face_mask = np.zeros(self._value.shape, dtype=np.bool8)
+        else:
+            fx, fy, fw, fh = face_utils.rect_to_bb(self._face)
+            # generate mask with face area masked
+            face_mask = np.zeros(self._value.shape, dtype=np.bool8)
+            face_mask[fy:fy+fh+1, fx:fx+fw+1] = 1
+        return face_mask
 
-        fx, fy, fw, fh = face_utils.rect_to_bb(self._faces[0])
-        cv2.rectangle(self._image, (fx, fy), (fx+fw, fy+fh), MAGENTA, 1)
-
-        # generate mask with face area masked
-        mask = np.zeros(array.shape, dtype=np.bool8)
-        mask[fy:fy+fh+1, fx:fx+fw+1] = 1
-        return mask
-
-    def _get_masked_array(self, array: NDArray) -> NDArray:
-        mask = self._generate_mask(array)
-        return ma.masked_array(array, mask)
+    def _get_value_with_face_masked(self) -> NDArray:
+        face_mask = self._generate_face_mask()
+        return ma.masked_array(self._value, face_mask)
 
     @staticmethod
-    def _filtered_mean(masked_array: NDArray) -> float:
-        """Filter out the brightest and darkest 5% area of a masked image."""
-        # compress the masked array to truncate masked constants
-        unmasked = ma.compressed(masked_array)
-        unmasked.sort()
-        return unmasked[int(unmasked.size * 0.05):int(unmasked.size * 0.95)].mean()
+    def _filtered_mean(array: NDArray[(Any,), Any]) -> float:
+        """Returns the mean of values with the brightest and darkest 5% area
+        filtered out.
+
+        Arguments:
+            array: A 1-D to calculate mean on.
+        """
+        array.sort()
+        return array[int(array.size * 0.05):int(array.size * 0.95)].mean()
+
+
+def plot_mask_diff(filter: ImageFilter) -> None:
+    """Plots the image and shows filtered brightness on the window."""
+    masked = filter.get_brightness()
+    without_mask = filter.get_brightness(mask=False)
+    diff = masked - without_mask
+
+    title = (f"Brightness without mask: {without_mask}\n"
+             f"Result brightness: {masked} (diff = {diff:.2f})")
+
+    plt.imshow(filter.image)
+    plt.title(title)
+    plt.show()
--- image_filter/main.py
+++ image_filter/main.py
@@ -1,17 +1,62 @@
 import cv2
+import dlib
+import imutils
 import matplotlib.pyplot as plt
+from imutils import face_utils
+from nptyping import Int, NDArray
 
-from image_filter.filter import ImageFilter
+from image_filter.filter import ImageFilter, plot_mask_diff
+from util.color import CYAN, MAGENTA
+from util.cv_font import FONT_0
+from util.image_type import ColorImage
 from util.path import to_abs_path
 
 
-if __name__ == "__main__":
-    dir = to_abs_path("image_filter")
-    ref_img = cv2.imread(dir + "/img/ref_img.jpg")
-    demo_img = cv2.imread(dir + "/img/demo.jpg")
-    image_1 = cv2.imread(dir + "/img/dark_room.jpg")
-    image_2 = cv2.imread(dir + "/img/dark_room_with_lightspot.jpg")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(
+"posture/trained_models/shape_predictor_68_face_landmarks.dat")
+filter = ImageFilter()
 
-    image_filter = ImageFilter()
-    image_filter.refresh_image(demo_img)
-    image_filter.plot_image()
+
+def real_time_brightness_capture() -> None:
+    cam = cv2.VideoCapture(0)
+
+    while cam.isOpened():
+        _, frame = cam.read()
+        frame = cv2.flip(frame, flipCode=1)
+        faces: dlib.rectangles = detector(frame)
+        if len(faces) == 0:
+            continue
+        # 1st is used
+        face: dlib.rectangle = faces[0]
+        filter.refresh_image(frame, face)
+        # draw face
+        fx, fy, fw, fh = face_utils.rect_to_bb(faces[0])
+        cv2.rectangle(frame, (fx, fy), (fx+fw, fy+fh), MAGENTA, 1)
+
+        brightness: float = filter.get_brightness()
+        cv2.putText(frame, f"Bright. = {brightness}", (50, 50), FONT_0, 1, CYAN, 2)
+        frame = imutils.resize(frame, width=600)
+        cv2.imshow("Brightness", frame)
+        if cv2.waitKey(100) & 0xFF == ord("q"):
+            break
+
+
+def show_static_diff(frame: ColorImage) -> None:
+    frame = cv2.flip(frame, flipCode=1)
+    faces: dlib.rectangles = detector(frame)
+    if len(faces) == 0:
+        print("no face")
+        return
+    # 1st is used
+    face: dlib.rectangle = faces[0]
+    filter.refresh_image(frame, face)
+    plot_mask_diff(filter)
+
+
+if __name__ == "__main__":
+    # real_time_brightness_capture()
+
+    dir = to_abs_path("image_filter")
+    frame = cv2.imread(dir + "/img/ref_img.jpg")
+    show_static_diff(frame)
