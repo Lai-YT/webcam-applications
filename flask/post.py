974ce1352f6cfffb31b60775b32e3189a3d7c646
MarsYang <ianfirst123@gmail.com>
2022-03-17 15:09:09 +0800

    Fix code of opening json file

--- flask/post.py
+++ flask/post.py
@@ -5,14 +5,14 @@ import time
 
 
 def main():
-    f = open("grade.json")
-    data = json.load(f)
-
-    while True:
-        # Post grades one by one every two seconds.
-        for grade in data["grades"]:
-            requests.post("http://127.0.0.1:5000/test", json=grade)
-            time.sleep(2)
+    with open("grade.json") as f:
+        data = json.load(f)
+    
+        while True:
+            # Post grades one by one every two seconds.
+            for grade in data["grades"]:
+                requests.post("http://127.0.0.1:5000/test", json=grade)
+                time.sleep(2)
 
 if __name__ == "__main__":
     main()
