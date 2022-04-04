4e4698f04fdf251bb1d5ce3365d336799dc0cbb4
Lai-YT <381xvmvbib@gmail.com>
2022-04-04 16:53:44 +0800

    Remove unused import & update docs

--- util/task_worker.py
+++ util/task_worker.py
@@ -1,30 +1,21 @@
-import time
 from functools import partial
-from typing import Optional
+from typing import Callable
 
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from PyQt5.QtCore import QThread
 
 
 class TaskWorker(QThread):
     """Takes a task function and arbitrary number of that function's parameters
     and runs the task function in a new thread.
     """
-    def __init__(self, task_callback, *args, **kwargs):
+    def __init__(self, task_callback: Callable, *args, **kwargs) -> None:
         super().__init__()
         self._task_callback = partial(task_callback, *args, **kwargs)
 
     # Override
-    def run(self) -> QThread:
+    def run(self) -> None:
         """Runs the worker in a new thread.
 
-        This is a convenient methods that allows you to not manually
-        handle the boilerplate.
-
-        Arguments:
-            stop_signal:
-                If provided, its emission quits the thread and stops the work.
-
-        Returns:
-            The thread which this worker is running in.
+        This method is called indirectly by QThread.start().
         """
         self._task_callback()
