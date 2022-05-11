8d8f6a5f75f6ef16a20e195641c95207b241fc5f
Lai-YT <381xvmvbib@gmail.com>
2022-05-11 11:39:35 +0800

    Use rotating log file
    Avoid giant log file.

--- util/logger.py
+++ util/logger.py
@@ -2,6 +2,7 @@
 #   https://stackoverflow.com/questions/65461959/calling-a-static-method-with-self-vs-class-name
 
 import logging
+import logging.handlers
 
 
 formatter = logging.Formatter("%(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
@@ -14,7 +15,8 @@ def setup_logger(name: str, log_file: str, level=logging.INFO) -> logging.Logger
         log_file: Where the logger logs to.
         level: Logging messages which are less severe than level will be ignored.
     """
-    handler = logging.FileHandler(log_file)
+    handler = logging.handlers.RotatingFileHandler(
+        log_file, maxBytes=6_000, backupCount=1)
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
