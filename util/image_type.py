12da006d164c3a47a8bde5cbbe09bfbc7b1c465e
Lai-YT <381xvmvbib@gmail.com>
2022-09-06 13:37:54 +0800

    Update type hints of numpy array
    BREAKING CHANGE:
    A newer version of "nptyping" is used and the old types are not
    compatible anymore. We'll go through the packages as we add tests
    and fix the type hints.

--- util/image_type.py
+++ util/image_type.py
@@ -7,6 +7,10 @@ from nptyping import NDArray, UInt8
 # A colored image has 3 dimensions. The third dimension is 3 (color channels).
 # Gray image has 2 dimensions.
 # Their integer types are both numpy.uint8
+#
+# The actual type under "nptyping" is as the comments behind.
+# I'm not using that because they are not compatible with cv2,
+# and VS Code shows the type as "Shape" instead of the shape of array.
 
-ColorImage = NDArray[(Any, Any, 3), UInt8]
-GrayImage = NDArray[(Any, Any), UInt8]
+ColorImage = NDArray[Any, UInt8]  # NDArray[Shape["*, *, 3"], UInt8]
+GrayImage = NDArray[Any, UInt8]  # NDArray[Shape["*, *"], UInt8]
